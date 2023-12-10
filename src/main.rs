02618cc5f7fbbb7fa8cbcb422688e56742dc9c92
Mohammed Yussif <mohammed.yussif@invennico.com>
2023-12-10 15:57:02 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::error::Error;
+
+use colour::{dark_green, yellow};
+use serde::Deserialize;
+
+
+#[derive(Deserialize, Debug)]
+struct  Articles {
+    articles: Vec<Article>
+}
+
+
+#[derive(Deserialize, Debug)]
+struct Article {
+    title: String, 
+    url: String
+}
+
+
+fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
+
+    let response = ureq::get(url).call()?.into_string()?;
+    let articles: Articles = serde_json::from_str(&response)?;
+
+    Ok(articles)
+}
+
+
+fn render_articles(articles: &Articles) {
+    for i in &articles.articles {
+        dark_green!("> {}\n", i.title);
+        yellow!("> {}\n", i.url);
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=789c58dc4026459fb27b1f142a9844f1";
+    let articles = get_articles(url)?;
+
+    render_articles(&articles); // Call the correct function name 'render_articles'
+
+    Ok(())
+}
