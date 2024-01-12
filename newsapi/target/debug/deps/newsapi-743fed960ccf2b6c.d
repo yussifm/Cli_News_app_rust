f2c354ea61f373fb1ef8af9fe5a7c126acad8a33
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-01-12 00:19:30 +0000

    .

---
+++ newsapi/target/debug/deps/newsapi-743fed960ccf2b6c.d
@@ -0,0 +1,5 @@
+D:\programming\pratice_area\Rust\clinews_app\newsapi\target\debug\deps\libnewsapi-743fed960ccf2b6c.rmeta: src\lib.rs
+
+D:\programming\pratice_area\Rust\clinews_app\newsapi\target\debug\deps\newsapi-743fed960ccf2b6c.d: src\lib.rs
+
+src\lib.rs:
